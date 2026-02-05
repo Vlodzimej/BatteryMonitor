3426f982010debfce4a5da853ffeb66800ef0d39
Vladimir Amelkin <v.amelkin@scloud.ru>
2026-02-05 22:20:17 +0300

    Initial Commit

---
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryMonitorApp.swift
+//  BatteryMonitor
+//
+//  Created by Vladimir Amelkin on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
